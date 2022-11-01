a6a530e20fa4cb64d15e93096349aa803f4d8e0d
mohdbilal <mohdbilal3386@gmail.com>
2022-11-02 00:31:55 +0530

    Making the Realtime

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Fragment, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Route, Routes,  Navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Authentication from "./components/Authentication/Authentication";
 import Header from "./components/Layout/Header";
 import SentMailDetail from "./components/SentMailDetails/SentMailDetails";
@@ -9,15 +9,20 @@ import HomePage from "./pages/HomePage";
 import InboxPage from "./pages/InboxPage";
 import MailDetailPage from "./pages/MailDetailPage";
 import SentMailPage from "./pages/SentMailPage";
-import { FetchingDataInbox, FetchingSentMail } from "./store/compose/compose-actions";
+import {
+  FetchingDataInbox,
+  FetchingSentMail,
+} from "./store/compose/compose-actions";
 
 function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(FetchingDataInbox())
-    dispatch(FetchingSentMail())
-  },[dispatch])
+    setInterval(() => {
+      dispatch(FetchingDataInbox());
+      dispatch(FetchingSentMail());
+    }, 2000);
+  }, [dispatch]);
   return (
     <Fragment>
       {isLoggedIn && <Header />}
@@ -26,11 +31,15 @@ function App() {
           {!isLoggedIn && <Route path="/auth" element={<Authentication />} />}
           <Route path="/auth" element={<Navigate replace to="/" />} />
           {isLoggedIn && <Route path="/" element={<HomePage />} />}
-          {isLoggedIn && <Route path="/compose" element={<ComposePage/>} />}
+          {isLoggedIn && <Route path="/compose" element={<ComposePage />} />}
           {isLoggedIn && <Route path="/inbox" element={<InboxPage />} />}
           {isLoggedIn && <Route path="/sentMail" element={<SentMailPage />} />}
-          {isLoggedIn && <Route path="/sentEmailDetails" element={<SentMailDetail />} />}
-          {isLoggedIn && <Route path="/emailDetails" element={<MailDetailPage />} />}
+          {isLoggedIn && (
+            <Route path="/sentEmailDetails" element={<SentMailDetail />} />
+          )}
+          {isLoggedIn && (
+            <Route path="/emailDetails" element={<MailDetailPage />} />
+          )}
         </Routes>
       </main>
     </Fragment>
